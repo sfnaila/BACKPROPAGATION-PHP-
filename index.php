200e7ab9a23c7610979396e6dc22e8a10be8a5d3
sfnaila <naila0604@gmail.com>
2016-09-28 13:14:05 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head><h1><center> BACKPROPAGATION (PHISHING WEBSITE) </center></h1></head>
+<body>
+
+<center><form action="process.php" method="post"/>
+HIDDEN LAYER <input type="text" name="hidden" required placeholder />
+EPOCH <input type="text" name="epoch" required placeholder />
+LEARNING RATE 1 <input type="text" name="lr1" required placeholder />
+LEARNING RATE 2 <input type="text" name="lr2" required placeholder />
+<input type="submit" name="submit" value="CALCULATE"  /></center>
+</form>
+
+</body>
+
+</html>
